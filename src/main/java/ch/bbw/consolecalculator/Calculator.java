576f81663587bb8a241ed17ce3130c82103e8df4
Samuel Dubler <samuel.dubler@hotmail.com>
2018-12-03 10:53:57 +0100

    Prepare test for subtraktion

--- src/main/java/ch/bbw/consolecalculator/Calculator.java
+++ src/main/java/ch/bbw/consolecalculator/Calculator.java
@@ -4,7 +4,8 @@ public class Calculator {
 	public int summe(int summand1, int summand2) {
 		return summand1 + summand2;
 	}
-//	public int subtraktion(int minuend, int subtrahend) {
+	public int subtraktion(int minuend, int subtrahend) {
 //		return minuend - subtrahend;
-//	}
+		return 0;
+	}
 }
