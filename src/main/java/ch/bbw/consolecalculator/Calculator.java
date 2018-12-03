abe6f9afded9d014b9868745677d949ae65739ca
5ia17sadubler <5ia17sadubler@gibwintra.ch>
2018-12-03 09:08:50 +0100

    Implement Addition

---
+++ src/main/java/ch/bbw/consolecalculator/Calculator.java
@@ -0,0 +1,10 @@
+package ch.bbw.consolecalculator;
+
+public class Calculator {
+	public int summe(int summand1, int summand2) {
+		return summand1 + summand2;
+	}
+	public int subtraktion(int minuend, int subtrahend) {
+		return minuend - subtrahend;
+	}
+}
