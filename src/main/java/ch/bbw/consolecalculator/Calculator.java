d3d3ee01f64556fa117a9f839a0a17fdd4b36d71
Samuel Dubler <samuel.dubler@hotmail.com>
2019-01-20 18:27:10 +0100

    Addition and Subtraction shall throw Arithmetic exceptions if positive or negative overflow

--- src/main/java/ch/bbw/consolecalculator/Calculator.java
+++ src/main/java/ch/bbw/consolecalculator/Calculator.java
@@ -1,10 +1,18 @@
 package ch.bbw.consolecalculator;
 
 public class Calculator {
-	public int summe(int summand1, int summand2) {
+	public int summe(int summand1, int summand2) throws java.lang.ArithmeticException{
+		long value = (long) summand1 + (long) summand2;
+		if((value > Integer.MAX_VALUE)||(value < Integer.MIN_VALUE)) {
+			throw new java.lang.ArithmeticException();
+		}
 		return summand1 + summand2;
 	}
-	public int subtraktion(int minuend, int subtrahend) {
+	public int subtraktion(int minuend, int subtrahend) throws java.lang.ArithmeticException {
+		long value = (long) minuend - (long) subtrahend;
+		if((value > Integer.MAX_VALUE)||(value < Integer.MIN_VALUE)) {
+			throw new java.lang.ArithmeticException();
+		}
 		return minuend - subtrahend;
 	}
 	public double division(int a, int b) {
